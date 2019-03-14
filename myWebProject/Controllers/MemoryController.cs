5b1dddcbae2758d211ac7c52098433a7ea113eb6
peter1998 <pmatov@gmail.com>
2019-03-14 08:54:22 +0200

    Add

---
+++ myWebProject/Controllers/MemoryController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using myWebProject.Models;
+namespace myWebProject.Controllers
+{
+    public class MemoryController : Controller
+    {
+        public IActionResult Pepi(int total)
+        {
+            var list = new List<int>();
+
+            for (int i = 0; i < total; i++)
+            {
+                list.Add(i + 1);
+            }
+
+            return View(list);
+        }
+    }
+}
---
+++ myWebProject/Models/Memory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace myWebProject.Models
+{
+    public class Memory
+    {
+        public string Summer { get; set; }
+
+        public int Year { get; set; }
+    }
+}
